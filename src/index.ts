dcf1d7e6e560235dfeb6a834b40bcddcb39e1bd0
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-25 21:33:59 +0100

    user error

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+console.log('opening the console is user error');
+
+const action = document.getElementById('action')!;
+const grace = 1500;
+
+setTimeout(() => {
+  action.innerText = 'doing nothing is';
+
+  document.addEventListener('keypress', (e) => {
+    action.innerText = `pressing ${e.key} is`;
+  });
+  
+  document.addEventListener('mousemove', (e) => {
+    if (e.movementX > 40 || e.movementX > 40)
+    action.innerText = 'moving the mouse too quickly is';
+  });
+  
+  document.addEventListener('click', (e) => {
+    let button;
+    switch (e.button) {
+      case 0:
+        button = 'left';
+        break;
+      case 1:
+        button = 'middle';
+        break;
+    }
+    action.innerText = `${button} clicking is`;
+  });
+  
+  document.addEventListener('contextmenu', () => {
+    action.innerText = 'right clicking is';
+  });
+}, grace);
+
+document.getElementById('language')!.addEventListener('click', () => {
+  // Prevent left click overwriting it
+  setTimeout(() => {
+    action.innerText = `${window.navigator.language} is`
+  }, 10)
+})
+
+
+window.addEventListener('beforeunload', (e) => {
+  e.preventDefault();
+  action.innerText = 'closing the tab is'
+  e.returnValue = '';
+})
