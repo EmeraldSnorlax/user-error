f2dfb5d03f0da8c73da023f29d2e6f4753bff1ac
d1snin <~@d1s.uno>
2021-12-29 15:36:50 +0300

    removed whitespace in cookie value

--- src/_index.ts
+++ src/_index.ts
@@ -28,4 +28,4 @@ setInterval(() => {
 
 // eslint-disable-next-line no-console
 console.log('opening the console is user error');
-document.cookie = 'user_error=Browsing cookies is user error.'
+document.cookie = 'user_error=Browsing_cookies_is_user_error.'
